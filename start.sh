bd96956fac69126874794ef334219e14f9c779bd
reivhax <xavierkibet@gmail.com>
2019-03-22 01:14:44 +0300

    update start script

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
+# list apps
+export apps=$(./manage.py shell -c "from django.conf import settings;print(' '.join(x for x in settings.INSTALLED_APPS if '.' not in x))")
+# make migrations
+python manage.py makemigrations $apps
+# migrate
+python manage.py migrate
 # start server
 gunicorn portfolio.wsgi --workers 4
