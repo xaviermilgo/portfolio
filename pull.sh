afd65a32fd89832ff5163423b27d887ccaa38b9d
reivhax <xavierkibet@gmail.com>
2019-03-22 01:23:27 +0300

    add virtual support on start script

--- pull.sh
+++ pull.sh
@@ -1,6 +1,18 @@
+# create virtual
+python3 -m venv virtual
+# activate virtual
+source virtual/bin/activate
 # stop server
 pkill gunicorn
 # pull code
 git pull origin master
+# update requirements
+pip install -r requirements.txt
+# list apps
+export apps=$(./manage.py shell -c "from django.conf import settings;print(' '.join(x for x in settings.INSTALLED_APPS if '.' not in x))")
+# make migrations
+python manage.py makemigrations $apps
+# migrate
+python manage.py migrate
 # start server
 bash start.sh &
