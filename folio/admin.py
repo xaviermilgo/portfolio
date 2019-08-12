309c211d0ce491152273fd39985661636ad089aa
reivhax <xavierkibet@gmail.com>
2019-08-12 14:40:27 +0300

    remove ignored folder

--- folio/admin.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from .models import Project, Skill, Visit
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(Project)
-admin.site.register(Skill)
-admin.site.register(Visit)
